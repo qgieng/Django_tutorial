98876d18a92352ffd585a37ab2284782a9b36be7
Quay Gieng <quaygieng@gmail.com>
2019-05-21 16:01:03 -0700

    login

--- login/urls.py
+++ login/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name='login'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('process/', views.login, name='loginp')
 ]
--- login/views.py
+++ login/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
 from django.template import loader
 from django.urls import reverse
@@ -7,4 +7,10 @@ from django.urls import reverse
 # Create your views here.
 def index(request):
     return render(request, 'login/index.html')
-    
+def login(request):
+    u = request.POST['username']
+    p = request.POST['password']
+    stuff = {'username':u,
+        'password':p
+    }
+    return render(request, 'login/main.html', stuff)
